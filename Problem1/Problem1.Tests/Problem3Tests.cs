fe0f2b745af47fa3808f420b9eca97f0d42d0b39
Dr. Klitty <mklittgaard@1800contacts.com>
2015-08-10 23:14:23 -0600

    add tests and finish 3

---
+++ Problem1/Problem1.Tests/Problem3Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Problems.Tests
+{
+    /*
+    The prime factors of 13195 are 5, 7, 13 and 29.
+
+    What is the largest prime factor of the number 600851475143
+
+    */
+
+    public class Problem3Tests
+    {
+        [Theory]
+        [InlineData(10, 5)]
+        [InlineData(13195, 29)]
+        [InlineData(600851475143, 6857)]
+        public void ReturnsCorrectPrimeFactors(long topNumber, long expected)
+        {
+            var classUnderTest = new Problem3();
+
+            var actual = classUnderTest.CalculatePrimeNumbers(topNumber);
+
+            Assert.Equal(expected,actual);
+        }
+    }
+}
---
+++ Problem1/Problem1/Problem3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Problem3
+    {
+        public long CalculatePrimeNumbers(long topNumber)
+        {
+            long largestFactor = 0;
+            long counter = 2;
+            long newNumber = topNumber;
+
+            while (counter * counter <= newNumber)
+            {
+                if (newNumber % counter == 0)
+                {
+                    newNumber = newNumber / counter;
+                    largestFactor = newNumber;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+
+            if (newNumber > largestFactor)
+            {
+                largestFactor = newNumber;
+            }
+
+            return largestFactor;
+        }
+    }
+}
